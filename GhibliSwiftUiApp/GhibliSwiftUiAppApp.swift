f118bb30e025130047fa1eae747f9c1209022f64
Dmitrii Ivchenko <ivchenkodmitri@gmail.com>
2025-12-03 20:00:08 +0300

    Initial Commit

---
+++ GhibliSwiftUiApp/GhibliSwiftUiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhibliSwiftUiAppApp.swift
+//  GhibliSwiftUiApp
+//
+//  Created by dimss on 03/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct GhibliSwiftUiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GhibliSwiftUiAppTests/GhibliSwiftUiAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GhibliSwiftUiAppTests.swift
+//  GhibliSwiftUiAppTests
+//
+//  Created by dimss on 03/12/2025.
+//
+
+import Testing
+@testable import GhibliSwiftUiApp
+
+struct GhibliSwiftUiAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
